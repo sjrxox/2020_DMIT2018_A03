e4754007d7b3663a780c7832ccb60c41420bd01e
sjrxox <46492908+sjrxox@users.noreply.github.com>
2020-01-09 09:37:52 -0700

    review context and controller classes, introduction to poco and dto data classes

---
+++ ChinookSolution/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,29 @@
+﻿using ChinookSystem.DAL;
+using ChinookSystem.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.BLL
+{
+    public class AlbumController
+    {
+        public List<Album> Album_List()
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Albums.ToList();
+            }
+        }
+
+        public Album Album_FindByID(int albumid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Albums.Find(albumid);
+            }
+        }
+    }
+}
