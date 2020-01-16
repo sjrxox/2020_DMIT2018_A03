d8c5603a25b853b967c98dcce8c466108027b355
sjrxox <46492908+sjrxox@users.noreply.github.com>
2020-01-16 08:48:45 -0700

    Added solution

---
+++ ChinookSolution/ChinookSystem/BLL/ArtistController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+#region Additional Namespaces
+using ChinookSystem.Data.Entities;
+using ChinookSystem.DAL;
+using System.ComponentModel;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class ArtistController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Artist> Artist_List()
+        {
+            using(var context = new ChinookContext())
+            {
+                return context.Artists.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public Artist Artist_FindByID(int artistid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Artists.Find(artistid);
+            }
+        }
+    }
+}
